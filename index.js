e9f35a8f5e3e5fa6b4ed96a4ef7e91464ea75bf9
ihatecsv <me@drakeluce.com>
2018-08-16 00:08:51 -0300

    Toying around with mining

--- index.js
+++ index.js
@@ -2,23 +2,35 @@ const Transaction = require("./Transaction");
 const Account = require("./Account");
 const Block = require("./Block");
 
+const diff = 3;
+
 const genesisBlock = new Block(null);
 
 let fakeTransactions = Transaction.generateFakeTransactions(6);
 
 genesisBlock.addTransactions(fakeTransactions);
 
-genesisBlock.setSolution(5);
-
-const firstBlock = new Block(genesisBlock);
-
-fakeTransactions = Transaction.generateFakeTransactions(2);
-
-firstBlock.addTransactions(fakeTransactions);
-
-const diff = 5;
-while(firstBlock.getHash().toString("hex").substr(0, diff) != "0".repeat(diff)){
-    firstBlock.setSolution(firstBlock.nonce + 1);
+console.log("Mining genesis");
+while(genesisBlock.getHash().toString("hex").substr(0, diff) != "0".repeat(diff)){
+    genesisBlock.setSolution(genesisBlock.nonce + 1);
 }
-console.log(firstBlock.getHash().toString("hex"));
-console.log(firstBlock.nonce);
+
+let lastBlock = genesisBlock;
+
+for(let i = 0; i < 10; i++){
+    const newBlock = new Block(lastBlock);
+
+    console.log("Mining height " + newBlock.height);
+
+    fakeTransactions = Transaction.generateFakeTransactions(2);
+    
+    newBlock.addTransactions(fakeTransactions);
+    
+    while(newBlock.getHash().toString("hex").substr(0, diff) != "0".repeat(diff)){
+        newBlock.setSolution(newBlock.nonce + 1);
+    }
+
+    lastBlock = newBlock;
+}
+
+console.log(lastBlock.getMineableData());
